7b110cde130c49f9eef93b396d6f879ceb8de98f
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-04 16:34:28 -0400

    tuples.py solution

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,7 +12,7 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-print('x is %d, y is %f, z is "%s"' % (x, round(y, 2), z))
+print('x is %d, y is %.2f, z is "%s"' % (x, round(y, 2), z))
 
 # Use the 'format' string method to print the same thing
 
